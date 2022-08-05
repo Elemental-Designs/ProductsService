428ff7cf841eb3cfdf5f05e10c55b2143dd9d88f
Jessica Chen <jessicachen012@gmail.com>
2022-08-05 10:48:33 -0700

    added local and deployed options

--- database/index.js
+++ database/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const { Pool, Client } = require('pg');
 
+// deployed connection
 const pool = new Pool({
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
@@ -8,6 +9,14 @@ const pool = new Pool({
   database: process.env.PGDATABASE,
   port: process.env.PGPORT
 });
+
+// local connection
+// const pool = new Pool({
+//   user: 'jessicachen',
+//   password: 'psql',
+//   host: 'localhost',
+//   database: 'atelier'
+// });
 
 module.exports = (text, values) => {
   return pool.connect()
